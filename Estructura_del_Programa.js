e44e0d20ac75492cd9e2c124272719093c276889
Fatima J. Gloria <99113573+FatimaGloria@users.noreply.github.com>
2024-10-16 21:50:07 -0600

    Create Estructura_del_Programa.js
    Se crea archivo capitulo 2

---
+++ Estructura_del_Programa.js
@@ -0,0 +1,14 @@
+/* Haciendo un triángulo con bucles
+Escribe un bucle que realice siete llamadas a console.log para mostrar el siguiente triángulo:
+
+#
+##
+###
+####
+#####
+######
+#######
+
+Puede ser útil saber que puedes encontrar la longitud de una cadena escribiendo .length después de ella.
+*/
+
