81f5494764f52aa86598b4c49ac5ebbc05d77eeb
Fatima J. Gloria <99113573+FatimaGloria@users.noreply.github.com>
2024-10-18 23:42:58 -0600

    Update Estructura_del_Programa.js
    Se agrega respuesta

--- Estructura_del_Programa.js
+++ Estructura_del_Programa.js
@@ -1,4 +1,4 @@
-/* Haciendo un triángulo con bucles
+/* 1. Haciendo un triángulo con bucles
 Escribe un bucle que realice siete llamadas a console.log para mostrar el siguiente triángulo:
 
 #
@@ -11,4 +11,8 @@ Escribe un bucle que realice siete llamadas a console.log para mostrar el siguie
 
 Puede ser útil saber que puedes encontrar la longitud de una cadena escribiendo .length después de ella.
 */
+
+for (let index = "#"; index.length <= 7; index += "#") { 
+    console.log(index); 
+} 
 
